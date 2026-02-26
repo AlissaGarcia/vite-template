4eeff02943986fc8b9961fcdf250580f7db9703c
Alissa Garcia <118315759+AlissaGarcia@users.noreply.github.com>
2026-02-26 19:36:51 -0300

    atualizações

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,11 +1,13 @@
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+
+import { HeaderMegaMenu } from '@/components/Header/HeaerMegaMenu';
 import { Welcome } from '../components/Welcome/Welcome';
 
 export function HomePage() {
   return (
     <>
+    <HeaderMegaMenu />
       <Welcome />
-      <ColorSchemeToggle />
+     
     </>
   );
 }
