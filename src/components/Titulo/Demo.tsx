aaff8be46f938228c6d34b32612570796d7b2d3b
Alissa Garcia <118315759+AlissaGarcia@users.noreply.github.com>
2026-02-26 20:56:24 -0300

    Atualições

---
+++ src/components/Titulo/Demo.tsx
@@ -0,0 +1,10 @@
+import { Title } from '@mantine/core';
+
+export function Demo() {
+  return (
+    <>
+      <Title order={1} ta = 'center'>Alguns dos nossos Projetos</Title>
+     
+    </>
+  );
+}
--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,13 +1,21 @@
 
-import { HeaderMegaMenu } from '@/components/Header/HeaerMegaMenu';
-import { Welcome } from '../components/Welcome/Welcome';
-
+import { ArticlesCardsGrid } from '@/components/Card/ArticleCardGrid';
+import { ContactUs } from '@/components/Contato/ContactUs';
+import { HeaderMegaMenu } from '@/components/Header/HeaderMegaMenu';
+import { HeroImageBackground } from '@/components/Hero/HeroImageBackground';
+import { HeroText } from '@/components/HeroText/HeroText';
+import { Demo } from '@/components/Titulo/Demo';
 export function HomePage() {
   return (
     <>
     <HeaderMegaMenu />
-      <Welcome />
-     
+    <HeroImageBackground />
+    <HeroText/>
+    <Demo />
+    <ArticlesCardsGrid />
+    <ContactUs />
     </>
   );
 }
+
+
